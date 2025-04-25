2f70a109d375fa9cde4ebb6644d793afa8fda776
NourhanCS <norhanmohmoud.2004@gmail.com>
2025-04-25 03:44:10 +0300

    Add project files.

---
+++ E-Commerce.Web/Program.cs
@@ -0,0 +1,36 @@
+
+namespace E_Commerce.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            
+
+            #region Add services to the container.
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            #endregion
+
+            var app = builder.Build();
+
+
+            #region Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.MapControllers();
+
+            #endregion
+
+            app.Run();
+        }
+    }
+}

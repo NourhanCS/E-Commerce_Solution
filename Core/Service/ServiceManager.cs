0624d3460c3b414de8f7981b9b2b504436809600
NourhanCS <norhanmohmoud.2004@gmail.com>
2025-05-02 01:07:35 +0300

    07 - Service Manager

---
+++ Core/Service/ServiceManager.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using DomainLayer;
+using Service;
+using ServiceAbstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class ServiceManager(IUnitOfWork unitOfWork , IMapper mapper) : IServiceManager
+    {
+        private readonly Lazy<IProductService> _LazyProductService = new Lazy<IProductService>(() => new ProductService(unitOfWork,mapper));
+        public IProductService productServic => _LazyProductService.Value;
+    }
+}
--- E-Commerce.Web/Program.cs
+++ E-Commerce.Web/Program.cs
@@ -6,6 +6,8 @@ using Persistence;
 using Persistence.Data;
 using Persistence.Repositories;
 using Service.MappingProfiles;
+using ServiceAbstraction;
+using Services;
 using System.Threading.Tasks;
 
 namespace E_Commerce.Web
@@ -31,6 +33,7 @@ namespace E_Commerce.Web
             builder.Services.AddScoped<IDataSeeding,DataSeeding>();
             builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
             builder.Services.AddAutoMapper(typeof(Services.AssemplyReference).Assembly);
+            builder.Services.AddScoped<IServiceManager, ServiceManager>();
             #endregion
 
            var app = builder.Build();

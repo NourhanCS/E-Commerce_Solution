9da92631b25128c0c7a9d899a944f6a32f70410e
NourhanCS <norhanmohmoud.2004@gmail.com>
2025-05-01 04:33:55 +0300

    04 - UnitOfWork

---
+++ Core/DomainLayer/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using DomainLayer.Contracts;
+using DomainLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer
+{
+    public interface IUnitOfWork
+    {
+        IGenericRepository <TEntity, Tkey> GetRepository<TEntity,Tkey>()where TEntity : BaseEntity<Tkey>;
+        Task <int> SaveChangesAsync();
+    }
+
+}
